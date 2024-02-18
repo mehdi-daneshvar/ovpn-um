8ae9874d05f9d5797dfeb9e40cbeb7e3d25bbafe
Mehdi Daneshvar <m.daneshvar1367@gmail.com>
2024-02-19 02:02:14 +0330

    feat[server-springboot]: Init Server project.

---
+++ server/src/main/java/net/lashar/ovpnum/OvpnUMApplication.java
@@ -0,0 +1,11 @@
+package net.lashar.ovpnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvpnUMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OvpnUMApplication.class, args);
+    }
+}

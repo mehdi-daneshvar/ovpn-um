25bf2f22de82dad718ff2ec2e3d612c935fc0e15
Mehdi Daneshvar <m.daneshvar1367@gmail.com>
2024-02-19 17:18:56 +0330

    feat[server-api-city]: create add city api.

--- server/src/main/java/net/lashar/ovpnum/service/CityService.java
+++ server/src/main/java/net/lashar/ovpnum/service/CityService.java
@@ -1,9 +1,12 @@
 package net.lashar.ovpnum.service;
 
 import net.lashar.ovpnum.entity.City;
+import net.lashar.ovpnum.error.exception.CityNameExistsException;
 
 import java.util.List;
 
 public interface CityService {
     List<City> getAll();
+
+    void addCity(City city) throws CityNameExistsException;
 }
--- server/src/main/java/net/lashar/ovpnum/service/CityServiceImpl.java
+++ server/src/main/java/net/lashar/ovpnum/service/CityServiceImpl.java
@@ -1,6 +1,7 @@
 package net.lashar.ovpnum.service;
 
 import net.lashar.ovpnum.entity.City;
+import net.lashar.ovpnum.error.exception.CityNameExistsException;
 import net.lashar.ovpnum.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,25 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CityServiceImpl implements CityService{
+public class CityServiceImpl implements CityService {
     @Autowired
     private CityRepository cityRepository;
+
     @Override
     public List<City> getAll() {
         return cityRepository.findAll();
+    }
+
+    @Override
+    public void addCity(City city) throws CityNameExistsException {
+        City existingCity =
+                cityRepository.findByName(city.getName())
+                        .orElse(null);
+        if (existingCity == null) {
+            cityRepository.save(city);
+        } else
+            throw new CityNameExistsException(
+                    "شهرستان %s از قبل وجود دارد.".formatted(city.getName())
+            );
     }
 }

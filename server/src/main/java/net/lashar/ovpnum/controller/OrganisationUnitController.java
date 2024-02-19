019d4b933b8d8444363c0e35fd75575c7c86fc7d
Mehdi Daneshvar <m.daneshvar1367@gmail.com>
2024-02-19 19:22:08 +0330

    feat[server-api-organisation-unit]: added OrganisationUnit app api.

--- server/src/main/java/net/lashar/ovpnum/controller/OrganisationUnitController.java
+++ server/src/main/java/net/lashar/ovpnum/controller/OrganisationUnitController.java
@@ -1,8 +1,17 @@
 package net.lashar.ovpnum.controller;
 
+import net.lashar.ovpnum.entity.City;
 import net.lashar.ovpnum.entity.OrganisationUnit;
+import net.lashar.ovpnum.error.exception.CityNotExistsException;
+import net.lashar.ovpnum.error.exception.OrganisationUnitNameExistsException;
+import net.lashar.ovpnum.error.exception.OrganisationUnitNameNullException;
+import net.lashar.ovpnum.error.exception.OrganisationUnitParentNotExistsException;
+import net.lashar.ovpnum.model.OrganisationUnitModel;
+import net.lashar.ovpnum.service.CityService;
+import net.lashar.ovpnum.service.MessageService;
 import net.lashar.ovpnum.service.OrganisationUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +21,49 @@ import java.util.List;
 public class OrganisationUnitController {
     @Autowired
     private OrganisationUnitService organisationUnitService;
+    @Autowired
+    private CityService cityService;
+    @Autowired
+    private MessageService messageService;
 
     @GetMapping(value = {"", "/"})
     public List<OrganisationUnit> getAll() {
         return organisationUnitService.getAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add
+            (@RequestBody OrganisationUnitModel organisationUnitModel)
+            throws
+            OrganisationUnitNameNullException,
+            CityNotExistsException,
+            OrganisationUnitParentNotExistsException,
+            OrganisationUnitNameExistsException {
+        if (organisationUnitModel.getName() == null) {
+            throw new OrganisationUnitNameNullException("نام واحد سازمانی خالی است.");
+        } else if (organisationUnitModel.getName().isEmpty()) {
+            throw new OrganisationUnitNameNullException("نام واحد سازمانی خالی است.");
+        } else {
+            OrganisationUnit organisationUnit = new OrganisationUnit();
+            organisationUnit.setName(organisationUnitModel.getName());
+            City city = cityService.getCity(organisationUnitModel.getCity()).orElse(null);
+            if (city == null) {
+                throw new CityNotExistsException(
+                        "کد شهرستان %d پیدا نشد.".formatted(organisationUnitModel.getCity())
+                );
+            }
+            organisationUnit.setCity(city);
+            if (organisationUnitModel.getParent() != null) {
+                OrganisationUnit parent = organisationUnitService.getParent(organisationUnitModel.getParent()).orElse(null);
+                if (parent == null) {
+                    throw new OrganisationUnitParentNotExistsException(
+                            "واحد سازمانی %s زیرمجموعه هیچ سازمانی قرار نگرفت.".formatted(organisationUnitModel.getName())
+                    );
+                } else organisationUnit.setParent(parent);
+            }
+            organisationUnitService.addOrganisationUnit(organisationUnit);
+            String message = "واحد سازمانی %s اضافه شد.".formatted(organisationUnitModel.getName());
+            return messageService.ok(message);
+        }
     }
 }
--- server/src/main/java/net/lashar/ovpnum/service/OrganisationUnitService.java
+++ server/src/main/java/net/lashar/ovpnum/service/OrganisationUnitService.java
@@ -1,9 +1,15 @@
 package net.lashar.ovpnum.service;
 
 import net.lashar.ovpnum.entity.OrganisationUnit;
+import net.lashar.ovpnum.error.exception.OrganisationUnitNameExistsException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrganisationUnitService {
     List<OrganisationUnit> getAll();
+
+    Optional<OrganisationUnit> getParent(Long id);
+
+    void addOrganisationUnit(OrganisationUnit organisationUnit) throws OrganisationUnitNameExistsException;
 }

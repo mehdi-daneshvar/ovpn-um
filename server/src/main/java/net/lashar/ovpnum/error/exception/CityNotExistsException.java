b9fbdc046eca4f4815e048c550df4690d5a21806
Mehdi Daneshvar <m.daneshvar1367@gmail.com>
2024-02-19 19:08:11 +0330

    feat[server-api-city]: Create exceptions and handler for City and OrganisationUnit.

---
+++ server/src/main/java/net/lashar/ovpnum/error/exception/CityNotExistsException.java
@@ -0,0 +1,7 @@
+package net.lashar.ovpnum.error.exception;
+
+public class CityNotExistsException extends Exception {
+    public CityNotExistsException(String message) {
+        super(message);
+    }
+}
---
+++ server/src/main/java/net/lashar/ovpnum/error/exception/OrganisationUnitNameNullException.java
@@ -0,0 +1,7 @@
+package net.lashar.ovpnum.error.exception;
+
+public class OrganisationUnitNameNullException extends Exception {
+    public OrganisationUnitNameNullException(String message) {
+        super(message);
+    }
+}
